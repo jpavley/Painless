ddbfe10fb3d4053115df2b0b74aeb6ba371bc966
John Pavley <john@pavley.com>
2024-03-16 13:55:54 -0400

    Update painless_example1.py
    better comments

--- painless_example1.py
+++ painless_example1.py
@@ -2,11 +2,16 @@
 # A ridiculously deep dive into the the print function
 
 # Example 1
-# Printing a message to the terminal
+# Sending a message to something somewhere somehow
 
 # Minimum viable Python program
 def main(): # Define a function called main
-    print("Hello, World!") # Print a message to the terminal
+    print("Hello, World!") # Print a message passed in an arugment to the terminal
+
+# What is a program?
+# What is a function?
+# What is a argument?
+# What is a terminal?
 
 if __name__ == "__main__": # if run, run it as a script, else just load it as a module
     main() # Call the main function when the script is run
