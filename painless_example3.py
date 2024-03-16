5fbdca675ea2b46260a874dd98599855cc3079fb
John Pavley <john@pavley.com>
2024-03-16 13:56:43 -0400

    Update painless_example3.py
    use painless_example2 as a module

--- painless_example3.py
+++ painless_example3.py
@@ -1,10 +1,12 @@
 # Painless Python
 # A ridiculously deep dive into the the print function
+import painless_example2 as pe2
 
 # Example 3.1
 # Printing a message to the terminal using the input function
 name = input("What is your name? ")
-print("Hello, " + name + "!")
+pe2.message(greeting="Hi there my friend", name=name)
+#print("Hello, " + name + "!")
 
 # Example 3.2
 # Printing a message to the terminal using the input function and concatenation
