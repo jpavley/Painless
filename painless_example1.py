18d6ca9cc0159c5b29076e2708bcb96ff2ed760b
John Pavley <john@pavley.com>
2024-03-16 14:13:30 -0400

    Update painless_example1.py

--- painless_example1.py
+++ painless_example1.py
@@ -4,14 +4,19 @@
 # Example 1
 # Sending a message to something somewhere somehow
 
-# Minimum viable Python program
+# Minimum viable Python script
 def main(): # Define a function called main
     print("Hello, World!") # Print a message passed in an arugment to the terminal
 
-# What is a program?
+# What is a script?
 # What is a function?
 # What is a argument?
 # What is a terminal?
 
 if __name__ == "__main__": # if run, run it as a script, else just load it as a module
     main() # Call the main function when the script is run
+
+# What is a module vs a script?
+# What does "run it" mean?
+# What does "load it" mean?
+# What does "call" mean?
