cd72b185c93be9007b1b1c9ea6f92eaaf79af819
John Pavley <john@pavley.com>
2024-03-16 09:47:50 -0400

    Update painless_example1.py
    updated comments

--- painless_example1.py
+++ painless_example1.py
@@ -8,5 +8,5 @@
 def main():
     print("Hello, World!") # Print a message to the terminal
 
-if __name__ == "__main__":
+if __name__ == "__main__": # if run, run it as a script, else just load it as a module
     main() # Call the main function when the script is run
