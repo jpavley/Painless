a20687dc454cc819f8346c60dd1271dd0023c27c
John Pavley <john@pavley.com>
2024-03-16 19:01:37 -0400

    Update painless_example2.4.py

--- painless_example2.4.py
+++ painless_example2.4.py
@@ -11,11 +11,16 @@ def main():
 def message(greeting, name):
     print(f"{greeting}, {name}!")
     
+# One thing that is cool is we wrapped the print function in a function called "message".
+# This if we find something better than the print function we can change without changing 
+# the rest of our code.
+    
 # What else can we do with the with our message function?
-# change the greeting based on the time of day?
-# Get input from the user?
-# Print to a file?
-# Reuse the message function in another script?
+# - Change the greeting based on the time of day?
+# - Get input from the user?
+# - Print to a file?
+# - Reuse the message function in another script?
+# - Can we find a better way to "print" to the terminal and to files?
     
 if __name__ == "__main__":
     main()
