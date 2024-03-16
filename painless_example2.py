b93cd19805005c794b18b8292136dd27eef2d08e
John Pavley <john@pavley.com>
2024-03-16 11:49:23 -0400

    Update painless_example2.py
    moved examples to functions

--- painless_example2.py
+++ painless_example2.py
@@ -5,7 +5,8 @@ def main():
     hello1()
     hello2()
     hello3()
-    hello4()
+    message()
+    
 
 # Example 2.1
 # Printing a message to the terminal using a variable
@@ -14,22 +15,19 @@ def hello1():
     print("Hello, " + name + "!")
 
 # Example 2.2
-# Printing a message to the terminal using a variable and the f-string
+# Printing a message to the terminal using a variable and the f-string method
 def hello2():
     name = "Paul"
     print(f"Hello, {name}!")
 
 # Example 2.3
 # Printing a message to the terminal using a variable and the format method
-def hello3():
-    name = "George"
+def hello3(name="George"):
     print("Hello, {}!".format(name))
 
 # Example 2.4
 # Printing a message to the terminal using two variables and the format method
-def hello4():
-    greeting = "Goodbye"
-    name = "Ringo"
+def message(greeting="Goodbye", name="Ringo"):
     print("{}, {}!".format(greeting, name))
     
 if __name__ == "__main__":
