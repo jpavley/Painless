e45443fde02078a7f1762e2382339b8a4bf59136
John Pavley <john@pavley.com>
2024-03-16 14:14:52 -0400

    Create painless_example2.2.py

---
+++ painless_example2.2.py
@@ -0,0 +1,22 @@
+# Painless Python
+# A ridiculously deep dive into the the print function
+
+def main():
+    helloFString()
+    
+# Example 2.1 kinda sovled hard coding problem.
+# But now we have this extra space between the name and the exclamation point.
+# Another problem is that we might run out of arguments to pass to the print function.
+
+# Example 2.2
+# Printing a message to the terminal using a variable and the f-string method
+def helloFString():
+    name = "Paul"
+    print(f"Hello, {name}!")
+
+# What is a f-string?
+# What do the curly braces do?
+# How many arguments are passed to the print function?
+    
+if __name__ == "__main__":
+    main()
