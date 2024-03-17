5d4e520c461bd8163f7774b076296265b44a8a32
John Pavley <john@pavley.com>
2024-03-17 14:03:44 -0400

    Create painless_example3_4.py

---
+++ painless_example3_4.py
@@ -0,0 +1,36 @@
+# Painless Python
+# A ridiculously deep dive into the the print function
+import datetime as dt # Import the datetime module so we can tell time
+# import painless_example2_4 as pe2
+
+# Example 3.4
+# Print to a file
+def message(greeting, name, fileName):
+    if fileName:
+        file = open(fileName, "a")
+        print(f"{greeting}, {name}!", file=file)
+    else:
+        print(f"{greeting}, {name}!")
+
+def main():
+    # This is a conditional statement: if something is true do this, else do that
+    if dt.datetime.now().hour < 12:
+        greeting = "Good morning"
+    else:
+        greeting = "Good afternoon"
+    message(greeting=greeting, name="Aida", fileName=None)
+    message(greeting=greeting, name="Aida", fileName="painless_example3_4.txt")
+
+# What happens if fileName is ""?
+# Does the open function do?
+# What does the "a" mean?
+
+if __name__ == "__main__":
+    main()
+    
+# Message function checklist:
+# x Reuse the message function in another script
+# x Get input from the user
+# x Change the greeting based on the time of day
+# x Print to a file
+# - Can we find a better way to "print" to the terminal and to files
