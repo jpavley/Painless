1996fb1d3653e191d96b14dd28e4ccd6e54e77de
John Pavley <john@pavley.com>
2024-03-17 13:38:55 -0400

    added todo list

--- painless_example2_4.py
+++ painless_example2_4.py
@@ -16,11 +16,11 @@ def message(greeting, name):
 # the rest of our code.
     
 # What else can we do with the with our message function?
-# - Reuse the message function in another script?
-# - Get input from the user?
-# - Change the greeting based on the time of day?
-# - Print to a file?
-# - Can we find a better way to "print" to the terminal and to files?
+# - Reuse the message function in another script
+# - Get input from the user
+# - Change the greeting based on the time of day
+# - Print to a file
+# - Can we find a better way to "print" to the terminal and to files
     
 if __name__ == "__main__":
     main()
--- painless_example3_1.py
+++ painless_example3_1.py
@@ -12,6 +12,12 @@ def main():
     pe2.message(greeting="Hail elf friend", name="Frodo")
     pe2.message(greeting="You shall not pass", name="Balrog")
 
-    
 if __name__ == "__main__":
     main()
+    
+# Message function checklist:
+# x Reuse the message function in another script
+# - Get input from the user
+# - Change the greeting based on the time of day
+# - Print to a file
+# - Can we find a better way to "print" to the terminal and to files
