88d71b443fca2a6ca17a188ccbae2ecbbe5345c1
John Pavley <john@pavley.com>
2024-03-21 10:21:33 -0400

    Update painless_example4_4.py

--- painless_example4_4.py
+++ painless_example4_4.py
@@ -11,18 +11,22 @@ load_dotenv()
 # Retrieve the API key from environment variables
 OpenAI.api_key = os.getenv('OPENAI_API_KEY')
 
+# Get the OpenAI client
 client = OpenAI()
 
+# Get a response from OpenAI
 completion = client.chat.completions.create(
-  model="gpt-3.5-turbo",
+  # The model we want to use (to save money we could use gpt-3.5-turbo 
+  # but the response might not be as good as gpt-4)
+  model="gpt-4",
   messages=[
     {"role": "system", "content": "You are a research assistant, expert in the presidents of the united states and you have a dry sense of humor."},
-    {"role": "user", "content": "Create a table with the following information. The title of the table is 'American Presidents Birthday Briefing'. The subtitle is 'Updated as of {current month} {current year}'. Inside the braces are variables that you should replace with the current month the current year. For example 'Updated as of March 2024'. The column headers of the table should be 'Name', 'Birthday', and 'Fun Fact'. For each president born in the current month table will have a row corresponding to that presidents name, birthday, and a fun fact. The fun fact is an short and amusing anecdote about that president. The whole table should be written to a file in markdown format."}
+    {"role": "user", "content": "Create a table with the following information. The title of the table is 'American Presidents Birthday Briefing'. The subtitle is 'Updated as of {current month} {current year}'. Inside the braces are variables that you should replace with the current month the current year. For example 'Updated as of March 2024'. The column headers of the table should be 'Name', 'Birthday', and 'Fun Fact'. For each president born in the current month table will have a row corresponding to that presidents name, birthday, and a fun fact. The fun fact is an short and amusing anecdote about that president. The whole table should be written to a file in markdown format. Just provide the markdown, no comments."}
   ]
 )
 
 file_name = "painless_example4_4.md"
-print(completion.choices[0].message, file=open(file_name, "a"))
+print(completion.choices[0].message.content, file=open(file_name, "w"))
 
 # Monthly presidential brithday briefing todo list:
 # x Figure out how to create a markdown file that contains a chart
