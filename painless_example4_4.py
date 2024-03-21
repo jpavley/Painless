054db8c8dd21674b453e0959f4a4e432ce4f4180
John Pavley <john@pavley.com>
2024-03-21 11:24:21 -0400

    Update painless_example4_4.py

--- painless_example4_4.py
+++ painless_example4_4.py
@@ -18,6 +18,9 @@ from openai import OpenAI
 from dotenv import load_dotenv
 # I'm using the os library to load the environment variables
 import os
+# Let's reuse the file name generator from the previous example
+import painless_example4_3 as pe4_3
+import datetime
 
 # Load environment variables from .env file
 load_dotenv()
@@ -27,6 +30,10 @@ OpenAI.api_key = os.getenv('OPENAI_API_KEY')
 
 # Get the OpenAI client
 client = OpenAI()
+
+current_month_index = datetime.datetime.now().month
+current_month_name = pe4_3.get_month_from_int(current_month_index).strftime("%B")
+current_year = pe4_3.get_month_from_int(current_month_index).year
 
 # Get a response from OpenAI
 completion = client.chat.completions.create(
@@ -40,16 +47,15 @@ completion = client.chat.completions.create(
     # The system role explains to the model what kind of helper we want it to simulate
     {"role": "system", "content": "You are a research assistant, expert in the presidents of the united states and you have a dry sense of humor."},
     # The user roles asks the model what the task is
-    {"role": "user", "content": "Create a table with the following information. The title of the table is 'American Presidents Birthday Briefing'. The subtitle is 'Updated as of {current month} {current year}'. Inside the braces are variables that you should replace with the current month the current year. For example 'Updated as of March 2024'. The column headers of the table should be 'Name', 'Birthday', and 'Fun Fact'. For each president born in the current month table will have a row corresponding to that presidents name, birthday, and a fun fact. The fun fact is an short and amusing anecdote about that president. The whole table should be written to a file in markdown format. Just provide the markdown, no comments."}
+    {"role": "user", "content": f"Create a table with the following information. The title of the table is 'American Presidents Birthday Briefing'. The subtitle is 'Updated as of {current_month_name} {current_year}'. Inside the braces are variables that you should replace with the current month the current year. The column headers of the table should be 'Name', 'Birthday', and 'Fun Fact'. For each president born in the current month table will have a row corresponding to that presidents name, birthday, and a fun fact. Only include presidents born in in {current_month_name}. Do not include any presidents born in any other months. The fun fact is an short and amusing anecdote about that president. The whole table should be written as a file in markdown format. Just provide the markdown, no introduction, no comments, no notes, nothing outside the markdown."}
   ]
 )
 
 # The name of the markdown file
-file_name = "painless_example4_4.md"
+file_name = pe4_3.generate_unique_file_name(current_month_index)
 # Write the OpenAI response to a file
+# print(completion.choices[0].message.content)
 print(completion.choices[0].message.content, file=open(file_name, "w"))
-
-#print(completion.choices[0].message.content, file=open(file_name, "w"))
 
 # Monthly presidential brithday briefing todo list:
 # x Figure out how to create a markdown file that contains a chart
