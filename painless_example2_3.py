fc32bfb84a57adef523c009b0e7fb44209a620d2
John Pavley <jpavley@gmail.com>
2024-04-01 16:32:51 -0400

    Update painless_example2_3.py

--- painless_example2_3.py
+++ painless_example2_3.py
@@ -1,11 +1,12 @@
 # Painless Python
-# A ridiculously deep dive into the the print function
+# A ridiculously deep dive into Python by way of the the print function
 
 # Example 2.2 solved the extra space and the number of arguments problems
-# But we still have a hard coding problem: every time the hello_fstring 
-# function runs we get the same result.
+# But we still have problems:
+# - Every time the hello function runs we get the same result
+# - If we want to say hello to someone else we have to change the hello function
 
-# Let's see if we can make it even less hard coded
+# Let's see if we can make our script even less hard coded
 
 # Example 2.3
 # Printing a message to the terminal passing a argument when the function is
@@ -14,10 +15,11 @@
 def main():
     hello_argument(name="George") # Set the value of name when calling
     hello_argument(name="Susan")
-    hello_argument(name="Angel")
+    hello_argument(name="🤨")
+    hello_argument(name="こんにちは")
     
 def hello_argument(name):
-    print(f"Hello, {name}!") # Use the arguement as a variable
+    print(f"Hello, {name}!") # Use the parameter as a variable
 
 # The hello_argument function can say hello to anyone!
 
