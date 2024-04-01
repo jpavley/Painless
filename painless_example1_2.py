47be3d8efeec2761f4da05e7995b32c4268c5814
John Pavley <jpavley@gmail.com>
2024-04-01 15:50:24 -0400

    Create painless_example1_2.py

---
+++ painless_example1_2.py
@@ -0,0 +1,18 @@
+# Painless Python
+# A ridiculously deep dive into Python by way of the the print function
+
+# Example 1.2
+# Importing a script
+
+# import example 1.1 as a module
+import painless_example1 as pe1
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    print(__name__)
+    main()
+else:
+    print(__name__)
+    pass
