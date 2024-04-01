be2d7078146ac8dea6c8e677fea3708ee5b424d7
John Pavley <jpavley@gmail.com>
2024-04-01 16:24:03 -0400

    Update painless_example2_2.py

--- painless_example2_2.py
+++ painless_example2_2.py
@@ -1,30 +1,39 @@
 # Painless Python
-# A ridiculously deep dive into the the print function
+# A ridiculously deep dive into Python by way of the the print function
 
 def main():
     hello_fstring()
     
 # Example 2.1 started to sovle the hard coding problem
-# But now we have this extra space between the name and the exclamation point
-# Another problem is that we might run out of arguments to pass to the print 
-# function (is there a limit? Who knows but probably!)
+# But we have some problems...
+# - We have this extra space between the name and the exclamation point--that was unexpected
+# - We're passing 3 arguments to the print function: is there a limit? Who knows but probably!
 
 # Is there a way to merge the values of many variables into a single string?
 
 # Example 2.2
-# Printing a message to the terminal using a variable and the f-string 
-# technique
+# Printing a message to the terminal using a variable and the f-string technique
 
 def hello_fstring():
-    name = "Paul" # Set the value of name before we call print
-    print(f"Hello, {name}!") # Pass the variable as an arguement to print
+    name = "Nancy" # Set the value of name as we did before
+    print(f"Hello, {name}!") # Use the f-string to embed the value of "name" into a single argument
+
+    # Change the value of name and say hello to someone else
+    name = "John"
+    print(f"Hello, {name}!")
+
+    # Can a string include an emoji?
+    name = "🤨"
+    print(f"Hello, {name}!")
+
+    # Can a string include japanese characters?
+    name = "こんにちは"
+    print(f"Hello, {name}!")
 
 # What is a string?
 # What do the quoation marks do?
 # What is a f-string?
-# What does the f in front of the string do?
 # What do the curly braces do?
-# How many arguments are passed to the print function?
     
 if __name__ == "__main__":
     main()
